819447616709efc4e8d6b39a08082538c0023c1c
Jean Carlo Gómez Narváez <gomezjean266@gmail.com>
2023-10-04 18:35:13 -0500

    correcion fourth

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (tag) => {
-            return tag.startsWith('ion-') // (return true)
-          }
-        }
-      }
-    })
-  ]
+  plugins: [vue()],
 })
+
